38d112a08589e0434887080192492c97e483daa1
Brian R. Bondy <netzen@gmail.com>
2020-06-01 13:48:41 -0400

    Improve issue parsing
    So that it handles cases like Fix and then not an issue number, and also cases with Fix followed by a colon

--- brave/parsing.py
+++ brave/parsing.py
@@ -8,7 +8,8 @@ def get_closed_issue(body, default_repo_stub):
     if not m:
         print('no match')
         return None
-    closed_issue = m.group(2).strip()
+    # print('m.group2: \"', m.group(2) + '\"')
+    closed_issue = m.group(2).strip().lstrip(':').lstrip()
 
     closed_repo_stub = None
     closed_number = None
@@ -16,8 +17,11 @@ def get_closed_issue(body, default_repo_stub):
     # Check if we have something like Fix brave/brave-core#3131
     simple_syntax_split = closed_issue.split('#')
     if len(simple_syntax_split) == 2:
-        (closed_repo_stub, closed_number) = closed_issue.split('#')
+        (closed_repo_stub, closed_number) = simple_syntax_split
         closed_repo_stub = closed_repo_stub if bool(closed_repo_stub) else default_repo_stub
+        # Make sure it's not something like "Fix for #3131"
+        if len(closed_repo_stub.split('/')) != 2:
+            return None
 
     # Check if we have something like Fix https://github.com/brave/brave-core/issues/3131
     url_path_split = urlparse(closed_issue).path.split('/')
@@ -25,9 +29,9 @@ def get_closed_issue(body, default_repo_stub):
         closed_repo_stub = url_path_split[1] + '/' + url_path_split[2]
         closed_number = url_path_split[4]
     if not bool(closed_repo_stub):
-        print('closed repo stub is not valid:', body)
+        # print('closed repo stub is not valid:', body)
         return None
     if not bool(closed_number):
-        print('closed number is not valid:', body)
+        # print('closed number is not valid:', body)
         return None
-    return (closed_repo_stub, closed_number.split()[0])
+    return (closed_repo_stub, int(closed_number.split()[0]))
--- brave/test_parsing.py
+++ brave/test_parsing.py
@@ -2,25 +2,31 @@ from . import parsing
 
 
 def test_get_closed_issue():
-    data = 'Fix #3131\nxxxxx'
-    assert parsing.get_closed_issue(data, 'brave/brave-browser') == ('brave/brave-browser', '3131')
+    data = ' Fix: #3130\nxxxxx'
+    assert parsing.get_closed_issue(data, 'brave/brave-browser') == ('brave/brave-browser', 3130)
+
+    data = 'Resolves #3131\nxxxxx'
+    assert parsing.get_closed_issue(data, 'brave/brave-browser') == ('brave/brave-browser', 3131)
 
     data = ' Closes brave/brave-core#3132\nxxxxx fdsaf sad fasd fdsa '
-    assert parsing.get_closed_issue(data, 'brave/brave-browser') == ('brave/brave-core', '3132')
+    assert parsing.get_closed_issue(data, 'brave/brave-browser') == ('brave/brave-core', 3132)
 
     data = 'a CloseS brave/brave-core#3133\nxxxxx fdsaf sad fasd fdsa '
-    assert parsing.get_closed_issue(data, 'brave/brave-core') == ('brave/brave-core', '3133')
+    assert parsing.get_closed_issue(data, 'brave/brave-core') == ('brave/brave-core', 3133)
 
     data = 'Fix https://github.com/brave/brave-core/issues/3134'
-    assert parsing.get_closed_issue(data, 'brave/brave-core') == ('brave/brave-core', '3134')
+    assert parsing.get_closed_issue(data, 'brave/brave-core') == ('brave/brave-core', 3134)
 
     data = 'a Closes https://www.github.com/brave/brave-core/issues/3135 dsa\nxxxxx fdsaf sad fasd fdsa '
-    assert parsing.get_closed_issue(data, 'brave/brave-core') == ('brave/brave-core', '3135')
+    assert parsing.get_closed_issue(data, 'brave/brave-core') == ('brave/brave-core', 3135)
 
     data = 'a Closes https://www.github.com/brave/brave-core/issues/3136 dsa\nxxxxx fdsaf sad fasd fdsa '
-    assert parsing.get_closed_issue(data, 'brave/brave-core') == ('brave/brave-core', '3136')
+    assert parsing.get_closed_issue(data, 'brave/brave-core') == ('brave/brave-core', 3136)
 
     data = '''
     fdasfdsaa FiXeD https://www.github.com/brave/brave-core/issues/3136 dsa\nxxxxx fdsaf sad fasd fdsa
     '''
-    assert parsing.get_closed_issue(data, 'brave/brave-core') == ('brave/brave-core', '3136')
+    assert parsing.get_closed_issue(data, 'brave/brave-core') == ('brave/brave-core', 3136)
+
+    data = 'partial fix for #9063 requires https://github.com/brave/brave-browser/pull/9076'
+    assert parsing.get_closed_issue(data, 'brave/brave-core') is None
