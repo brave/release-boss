c69a9dc47e7f01746603a3934fb75ed97be86027
Brian R. Bondy <netzen@gmail.com>
2020-06-02 15:07:48 -0400

    Make notify work with an audit log channel

--- brave/slack.py
+++ brave/slack.py
@@ -2,7 +2,7 @@ from slack import WebClient
 from slack.errors import SlackApiError
 
 
-def notify(api_key, channel, message):
+def notify_internal(api_key, channel, message):
     client = WebClient(token=api_key)
     try:
         response = client.chat_postMessage(
@@ -13,3 +13,8 @@ def notify(api_key, channel, message):
         # You will get a SlackApiError if "ok" is False
         print(f"Got an error: {e.response['error']}")
         return e.response["ok"] is False
+
+
+def notify_user(api_key, username, message):
+    notify_internal(api_key, '#release-boss-audit-log', '[For %s] %s' % (username, message))
+    notify_internal(api_key, username, message)
