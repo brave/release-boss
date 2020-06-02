f1107cd1599c146015fd5ebcfb4e5716c1e51ec3
Brian R. Bondy <netzen@gmail.com>
2020-06-02 15:08:17 -0400

    Make slack access token optional

--- main.py
+++ main.py
@@ -10,17 +10,16 @@ def main():
         pr-milestone: Output data for the PRs that are missing milestones
         issue-milestone: Output data for the issues that are missing milestones
         fix-milestone-prs: Output data for issues that have no milestone, but associated PR does
-        fix-missing-qa-flags: Outputs data for issues that are missing QA flags
-        ''')
+        fix-missing-qa-flags: Outputs data for issues that are missing QA flags''')
     args = parser.parse_args()
 
     assert "GITHUB_ACCESS_TOKEN" in os.environ, "Access token must be specified with GITHUB_ACCESS_TOKEN"
-    assert "SLACK_ACCESS_TOKEN" in os.environ, "Access token must be specified with SLACK_ACCESS_TOKEN"
 
     github_access_token = os.environ['GITHUB_ACCESS_TOKEN']
-    slack_access_token = os.environ['SLACK_ACCESS_TOKEN']
+    slack_access_token = os.environ['SLACK_ACCESS_TOKEN'] if 'SLACK_ACCESS_TOKEN' in os.environ else ''
 
-    # slack.notify(slack_access_token, '@bbondy', 'Test!')
+    if not slack_access_token:
+        print('Warning: SLACK_ACCESS_TOKEN not specified, so no Slack notifications will be given')
 
     if args.action == 'pr-per-release':
         util.get_pull_requests(github_access_token, "brave/brave-core")
@@ -31,7 +30,7 @@ def main():
     elif args.action == 'fix-milestone-prs':
         util.fix_milestone_prs(github_access_token, "brave/brave-core")
     elif args.action == 'fix-missing-qa-flags':
-        util.fix_missing_qa_flags(github_access_token, "brave/brave-browser")
+        util.fix_missing_qa_flags(slack_access_token, github_access_token, "brave/brave-browser")
     else:
         print('Not a valid command: ', args.action)
 
