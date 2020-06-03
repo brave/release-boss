de0f6242972ca2a607eaee052c0a4cac823836b2
Brian R. Bondy <netzen@gmail.com>
2020-06-03 09:48:12 -0400

    Improve message formatting for users with no GH name filled out

--- brave/messages.py
+++ brave/messages.py
@@ -1,20 +1,26 @@
+def format_name(closed_by_login, closed_by_name):
+    if closed_by_name:
+        return '%s (%s)' % (closed_by_login, closed_by_name)
+    return closed_by_login
+
+
 def missing_qa_flags(closed_by_login, closed_by_name, html_url):
     return (r'''
-Github user %s (%s) closed issue %s without the needed QA flags.
+Github user %s closed issue %s without the needed QA flags.
 
 Please specify either `QA/Yes` or `QA/No`.
 If you specify `QA/Yes`, then please also specify a test plan.
 
 Thank you!
-    ''' % (closed_by_login, closed_by_name, html_url)).strip()
+    ''' % (format_name(closed_by_login, closed_by_name), html_url)).strip()
 
 
 def missing_release_note_flags(closed_by_login, closed_by_name, html_url):
     return (r'''
-Github user %s (%s) closed issue %s without the needed release note flags.
+Github user %s closed issue %s without the needed release note flags.
 
 Please specify either `release-notes/include` or `release-notes/exclude`.
 If you specify `release-notes/include`, then please ensure the title of the issue is appropriate for release notes.
 
 Thank you!
-    ''' % (closed_by_login, closed_by_name, html_url)).strip()
+    ''' % (format_name(closed_by_login, closed_by_name), html_url)).strip()
