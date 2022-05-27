95872a9c3bd1503bad631951630adbfeb790aa49
Brian R. Bondy <netzen@gmail.com>
2022-05-27 11:11:40 -0400

    Add Jay Harris

--- brave/config.py
+++ brave/config.py
@@ -87,6 +87,7 @@ github_slack_map = {
     'Douglashdaniel': 'U01Q1DJGB39',
     'emerick': 'U9E5WAX34',
     'evq': 'U5N5PQACV',
+    'fallaciousreasoning': 'U03DL26644V',
     'fmarier': 'UFAJB5HB8',
     'Fanboynz': 'UK7MJJX44',
     'gdregalo': 'U3MLL9K8C',
